9f43a33d22db02f17dc9ed8baedbfadb2516f154
fl-cross <federico.l.cross@gmail.com>
2025-11-24 12:58:13 -0300

    First Seed

---
+++ src/test/java/ar/com/flcross/msbank/MsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.flcross.msbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
